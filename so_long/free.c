6ab75ddcb834223ccb8add5febc071bd8374424d
Muhammed Taha Samiloglu <msamilog@k1m31s06.42istanbul.com.tr>
2023-08-23 19:05:21 +0300

    updated so_long dir

---
+++ so_long/free.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msamilog <tahasamiloglu@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 09:46:13 by msamilog          #+#    #+#             */
+/*   Updated: 2023/08/23 18:06:39 by msamilog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_exit(char *msg)
+{
+	write(1, "Error\n", 6);
+	ft_putstr(msg);
+	exit(1);
+}
+
+void	ft_error(char *msg, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	write(1, "Error\n", 6);
+	ft_putstr(msg);
+	while (data->map[i])
+	{
+		free(data->map[i]);
+		i++;
+	}
+	free(data->map[i]);
+	exit(1);
+}
+
+int	ft_close_window(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->map[i])
+	{
+		free(data->map[i]);
+		i++;
+	}
+	free(data->map[i]);
+	exit(0);
+}
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -5,27 +5,80 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: msamilog <tahasamiloglu@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/14 02:16:43 by msamilog          #+#    #+#             */
-/*   Updated: 2023/08/14 15:43:12 by msamilog         ###   ########.fr       */
+/*   Created: 2023/08/18 09:46:13 by msamilog          #+#    #+#             */
+/*   Updated: 2023/08/23 19:04:13 by msamilog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <stdio.h>
-# include <stdlib.h>
 # include "minilibx/mlx.h"
 # include <fcntl.h>
-# include <math.h>
-# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef struct s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+# define PL_XPM "./images/char.xpm"
+# define EX_XPM "./images/exit.xpm"
+# define C_XPM "./images/coin.xpm"
+# define WALL_XPM "./images/white.xpm"
+# define G_XPM "./images/ground.xpm"
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*mlx_win;
+	void	*player;
+	void	*exit;
+	void	*wall;
+	void	*collectible;
+	void	*background;
+	int		map_x;
+	int		map_y;
+	char	**map;
+	char	**c_map;
+	int		col_count;
+	int		collectible_count_copy;
+	int		exit_valid;
+	int		player_collected;
+	int		img_x;
+	int		img_y;
+	int		p_len;
+	int		e_len;
+	int		player_x;
+	int		player_y;
+	int		exit_x;
+	int		exit_y;
+	int		player_step;
+	int		map_y_control;
+	char	*player_step_screen;
+}			t_data;
+
+void		image_to_screen(t_data *data);
+int			handle_key_press(int keycode, t_data *data);
+void		get_map(t_data *data, char *map_name, char *str);
+char		*get_next_line(int fd);
+void		step_counter(t_data *data);
+int			ft_strlen(char *string);
+char		*ft_strdup(char *s1);
+char		**ft_split(const char *s, char c);
+char		*ft_strjoin(char *s1, char *s2);
+void		element_check(t_data *data);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+int			find_player(t_data *data, char find);
+void		is_rectangular(t_data *data);
+void		image_to_screen(t_data *data);
+void		image_initilize(t_data *data);
+void		ft_error(char *msg, t_data *data);
+char		*ft_itoa(int n);
+void		invalid_mapname(char *mapname, t_data *data);
+void		wall_okk(t_data *data);
+int			another_coin(t_data *data);
+int			ft_close_window(t_data *data);
+void		step_counter(t_data *data);
+char		ft_coordinat(int x, int y, t_data *data);
+void		ft_exit(char *msg);
+void		ft_putstr(char *str);
 
 #endif
