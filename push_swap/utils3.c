8b7bd5e1922145ab4135a1e2f8166a3b9a760db0
Muhammed Taha Samiloglu <msamilog@k1m01s02.42istanbul.com.tr>
2023-12-12 16:32:54 +0300

    Added better move_count logic

--- push_swap/utils3.c
+++ push_swap/utils3.c
@@ -6,7 +6,7 @@
 /*   By: msamilog <tahasamiloglu@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 10:19:15 by msamilog          #+#    #+#             */
-/*   Updated: 2023/12/08 10:55:24 by msamilog         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:23:24 by msamilog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,26 @@ int	ft_abs(int n)
 int	get_move_count(t_stack *s, int i)
 {
 	int	move_count;
+	int	max_or_rot;
 
-	move_count = 0;
 	if (s->a_size - i > i)
-		move_count += i;
+		move_count = i;
 	else
-		move_count += s->a_size - i;
+		move_count = i - s->a_size;
 	if (is_b_min_max(s, s->stack_a[i]))
-		move_count += ft_abs(max_num_stack_b(s));
+		max_or_rot = max_num_stack_b(s);
 	else
-		move_count += ft_abs(find_rot_b(s, s->stack_a[i]));
+		max_or_rot = find_rot_b(s, s->stack_a[i]);
+	if ((move_count < 0 && max_or_rot < 0)
+		|| (move_count > 0 && max_or_rot > 0))
+	{
+		if (ft_abs(move_count) > ft_abs(max_or_rot))
+			move_count = ft_abs(move_count);
+		else
+			move_count = ft_abs(max_or_rot);
+	}
+	else
+		move_count = ft_abs(move_count) + ft_abs(max_or_rot);
 	return (move_count);
 }
 
